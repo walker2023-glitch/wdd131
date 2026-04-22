2c9277b488cf50db1dc61e0718b3d8aa3a701257
walker2023-glitch <dallaswalker1023@gmail.com>
2026-04-22 14:50:02 -0600

    Added JS finished CSS

---
+++ ponder/metatags/meta-css.js
@@ -0,0 +1,34 @@
+
+
+// variables vs constants
+//constant
+const PI = 3.1415926535;
+//variable
+let radius;
+
+radius = 3;
+
+let area = radius * radius * PI;
+
+
+console.log(area);
+
+
+radius = 20;
+
+area = radius * radius * PI;
+
+
+console.log(area);
+
+
+
+
+
+
+
+
+
+
+
+
