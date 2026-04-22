a860369717e26e2dcf7a2a8e40e3cbb1e502e0ba
walker2023-glitch <dallaswalker1023@gmail.com>
2026-04-22 14:59:31 -0600

    JS code finished

--- ponder/metatags/meta-css.js
+++ ponder/metatags/meta-css.js
@@ -23,12 +23,43 @@ console.log(area);
 
 
 
+radius = 50;
+
+area = radius * radius * PI;
+
+
+console.log(area);
+
+
+// type coersion
+
+const one = 1;
+const two = '2';
+
+let result = one * two;
+console.log(result);
+
+
+//use Number() to force it to become a number
+result = one + two;
+console.log(result);
+
+
+result = one + Number(two);
+console.log(result);
 
 
 
 
-
-
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
 
 
 
