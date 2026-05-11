4bfe8a1bdbaa0afc9f42664c5210f2bbc7c0e69d
walker2023-glitch <dallaswalker1023@gmail.com>
2026-05-11 14:44:20 -0600

    Did the new Monday ponder

---
+++ ponder/responsive/responsive.js
@@ -0,0 +1,23 @@
+const menu = document.querySelector('nav');
+const btn = document.querySelector('.menu-btn')
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('hide');
+    btn.classList.toggle('change');
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
