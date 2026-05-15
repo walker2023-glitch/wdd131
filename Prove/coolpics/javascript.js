e76889d7ce0b89c59266b17106f15d46a4da2ebe
walker2023-glitch <dallaswalker1023@gmail.com>
2026-05-15 08:01:15 -0600

    Finished assignment

---
+++ Prove/coolpics/javascript.js
@@ -0,0 +1,45 @@
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    //console.log(e.target)
+    //console.log(e.src);
+    const img = e.target;
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+
+    const full = src.replace('sm', 'full');
+    modalImage.src = full;
+    modalImage.alt = alt;
+
+    modal.showModal();
+    
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+          
+
+const menu = document.querySelector('nav');
+const btn = document.querySelector('.menu-btn')
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('hide');
+    btn.classList.toggle('change');
+} 
